f4b7b3ebcc0d070213c0a3785bba6d8d6426b7e1
Degia Parlopa <degia56.dp@gmail.com>
2026-09-06 22:48:46 +0700

    Mod : SkillsSection Bar, Icon Motion Animate,  etc

--- src/data/skills.js
+++ src/data/skills.js
@@ -4,12 +4,14 @@ export const skills = [
   // UI/UX
   {
     name: "Adobe XD",
+    logo: "https://img.icons8.com/color/96/adobe-xd.png",
     sub: "UI, UX, etc",
     level: 80,
     category: "UI/UX",
   },
   {
     name: "Figma",
+    logo: "https://img.icons8.com/color/96/figma.png",
     sub: "UI, UX, etc",
     level: 90,
     category: "UI/UX",
@@ -18,24 +20,28 @@ export const skills = [
   // Web Developer
   {
     name: "React Js",
+    logo: "https://img.icons8.com/color/96/react-native.png",
     sub: "Vite, Tailwindcss, Js, Ts",
     level: 25,
     category: "Web Dev",
   },
   {
-    name: "Laravel 12",
+    name: "Laravel",
+    logo: "https://img.icons8.com/stickers/96/laravel.png",
     sub: "PHP, Tailwindcss, React, Js, Css",
     level: 25,
     category: "Web Dev",
   },
   {
     name: "CodeIgniter",
+    logo: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/external-codeigniter-is-an-open-source-software-rapid-development-web-framework-logo-shadow-tal-revivo.png",
     sub: "PHP, Css, HTML, Js",
     level: 25,
     category: "Web Dev",
   },
   {
     name: "DBMS",
+    logo: "https://img.icons8.com/color/96/database.png",
     sub: "Mysql, MariaDB, PostgreeSQL, etc",
     level: 75,
     category: "Web Dev",
@@ -44,30 +50,35 @@ export const skills = [
   // Data Analyst
   {
     name: "SQL Database",
+    logo: "https://img.icons8.com/color/96/sql.png",
     sub: "Mysql, MariaDB, PostgreeSQL, etc",
     level: 75,
     category: "Data Analyst",
   },
   {
     name: "Python",
+    logo: "https://img.icons8.com/color/96/python.png",
     sub: "Data Clean, Analythical data",
     level: 50,
     category: "Data Analyst",
   },
   {
     name: "Python Lib",
+    logo: "https://img.icons8.com/color/96/pandas.png",
     sub: "Pandas, Numpy, Matplotlib, Seaborn",
     level: 50,
     category: "Data Analyst",
   },
   {
     name: "Excel Advanced",
+    logo: "https://img.icons8.com/color/96/microsoft-excel-2019.png",
     sub: "Power Query, Pivots, Data Graph",
     level: 80,
     category: "Data Analyst",
   },
   {
     name: "Data Visualization",
+    logo: "https://img.icons8.com/color/96/combo-chart.png",
     sub: "Excel Graph, Power BI, Tableu",
     level: 75,
     category: "Data Analyst",
@@ -76,24 +87,28 @@ export const skills = [
   // Graphics Design
   {
     name: "Adobe Photoshop",
+    logo: "https://img.icons8.com/color/96/adobe-photoshop.png",
     sub: "Poster, Flayer, Mockup, etc",
     level: 80,
     category: "Graphics Design",
   },
   {
     name: "Adobe Illustrator",
+    logo: "https://img.icons8.com/color/96/adobe-illustrator.png",
     sub: "Poster, Flayer, Mockup, etc",
     level: 80,
     category: "Graphics Design",
   },
   {
     name: "Affinity",
+    logo: "https://img.icons8.com/plasticine/100/affinity-designer.png",
     sub: "Poster, Flayer, Mockup, etc",
     level: 30,
     category: "Graphics Design",
   },
   {
     name: "Canva",
+    logo: "https://img.icons8.com/color/96/canva.png",
     sub: "Poster, Flayer, Mockup, etc",
     level: 50,
     category: "Graphics Design",
@@ -102,18 +117,21 @@ export const skills = [
   // Network
     {
     name: "Cisco Packet Tracer",
+    logo: "https://img.icons8.com/ios/50/cisco-packet-tracer.png",
     sub: "Network Simulation",
     level: 50,
     category: "Network",
   },
   {
     name: "Network Security",
+    logo: "https://img.icons8.com/color/96/security-checked.png",
     sub: "Routing, Subnetting, Firewall, IDS/IPS, VPN, etc",
     level: 50,
     category: "Network",
   },
   {
     name: "Server & Cloud Admin",
+    logo: "https://img.icons8.com/color/96/cloud.png",
     sub: "Linux, Windows Server, AWS, Azure",
     level: 50,
     category: "Network",
