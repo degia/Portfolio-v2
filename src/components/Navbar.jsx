6813e6048f18ff01c6e41fad88e7db36ba1d39e7
Degia Parlopa <degia56.dp@gmail.com>
2026-09-06 21:24:24 +0700

    Add : Foto profile, Experience Navbar Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const navItems = [
   { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
+  { name: "Experience", href: "#experience" },
   { name: "Projects", href: "#projects" },
   { name: "Certificate", href: "#certif" },
   { name: "Contact", href: "#contact" },
